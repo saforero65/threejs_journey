a28c6754f44f44869d6e21ecc7991ce6484c011a
santiago <saforero88@gmail.com>
2025-01-06 21:42:00 -0500

    Add lesson 30: Implement animated galaxy with customizable parameters and shaders

---
+++ lessons/30-animated-galaxy/src/shaders/galaxy/fragment.glsl
@@ -0,0 +1,20 @@
+ varying vec3 vColor;
+
+ void main() {
+    #include <colorspace_fragment>
+
+    // Diffuse point
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+    // strength *= 2.0;
+    // strength = 1.0 - strength;
+
+    //Light point
+    float strength = distance(gl_PointCoord, vec2(0.5));
+    strength = 1.0 - strength;
+    strength = pow(strength, 10.0);
+
+    // Final color
+    vec3 color = mix(vec3(0.0), vColor, strength);
+
+    gl_FragColor = vec4(color, 1.0);
+}

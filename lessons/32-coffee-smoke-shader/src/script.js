05c89d2f7a9c0448a201e54afcab119c0eca025e
santiago <saforero88@gmail.com>
2025-02-09 20:01:17 -0500

    Add lesson 32: Implement coffee smoke shader with interactive controls and Perlin noise

---
+++ lessons/32-coffee-smoke-shader/src/script.js
@@ -0,0 +1,139 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import coffeeSmokeVertexShader from './shaders/coffeeSmoke/vertex.glsl'
+import coffeeSmokeFragmentShader from './shaders/coffeeSmoke/fragment.glsl'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Loaders
+const textureLoader = new THREE.TextureLoader()
+const gltfLoader = new GLTFLoader()
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 8
+camera.position.y = 10
+camera.position.z = 12
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.target.y = 3
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Model
+ */
+gltfLoader.load(
+    './bakedModel.glb',
+    (gltf) =>
+    {
+        gltf.scene.getObjectByName('baked').material.map.anisotropy = 8
+        scene.add(gltf.scene)
+    }
+)
+
+/**
+ * Smoke
+ */
+// Geometry
+const smokeGeometry = new THREE.PlaneGeometry(1, 1, 16, 64)
+smokeGeometry.translate(0, 0.5, 0)
+smokeGeometry.scale(1.5, 6, 1.5)
+
+// Perlin texture
+const perlinTexture = textureLoader.load('./perlin.png')
+perlinTexture.wrapS = THREE.RepeatWrapping
+perlinTexture.wrapT = THREE.RepeatWrapping
+
+// Material
+const smokeMaterial = new THREE.ShaderMaterial({
+    vertexShader: coffeeSmokeVertexShader,
+    fragmentShader: coffeeSmokeFragmentShader,
+    uniforms:
+    {
+        uTime: new THREE.Uniform(0),
+        uPerlinTexture: new THREE.Uniform(perlinTexture)
+    },
+    side: THREE.DoubleSide,
+    transparent: true,
+    depthWrite: false
+    // wireframe: true
+})
+
+// Mesh
+const smoke = new THREE.Mesh(smokeGeometry, smokeMaterial)
+smoke.position.y = 1.83
+scene.add(smoke)
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update smoke
+    smokeMaterial.uniforms.uTime.value = elapsedTime
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
---
+++ lessons/32-coffee-smoke-shader/src/shaders/coffeeSmoke/vertex.glsl
@@ -0,0 +1,33 @@
+uniform float uTime;
+uniform sampler2D uPerlinTexture;
+
+varying vec2 vUv;
+
+#include ../includes/rotate2D.glsl
+
+void main()
+{
+    vec3 newPosition = position;
+
+    // Twist
+    float twistPerlin = texture(
+        uPerlinTexture,
+        vec2(0.5, uv.y * 0.2 - uTime * 0.005)
+    ).r;
+    float angle = twistPerlin * 10.0;
+    newPosition.xz = rotate2D(newPosition.xz, angle);
+
+    // Wind
+    vec2 windOffset = vec2(
+        texture(uPerlinTexture, vec2(0.25, uTime * 0.01)).r - 0.5,
+        texture(uPerlinTexture, vec2(0.75, uTime * 0.01)).r - 0.5
+    );
+    windOffset *= pow(uv.y, 2.0) * 10.0;
+    newPosition.xz += windOffset;
+
+    // Final position
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+
+    // Varyings
+    vUv = uv;
+}
